325a29be5f594868b355e231b77d5e516a1f029b
geta6 <getakura@gmail.com>
2014-01-06 15:59:14 +0900

    ( ꒪⌓꒪) utf8

--- test.pl
+++ test.pl
@@ -1,5 +1,14 @@
+#!/usr/bin/env perl
+
+use utf8;
+use strict;
+use warnings;
 use Acme::Nyaa;
 
-my $n = Acme::Nyaa->new;
+my $kijitora = Acme::Nyaa->new;
 
-print $n->cat('後は野となれ山となれ');
+print $kijitora->cat("後は野となれ山となれ\n");
+
+print $kijitora->neko("神と和解せよ\n");
+
+print $kijitora->cat("明日の京都は雪でしょう」「最低気温は-4度です」");
