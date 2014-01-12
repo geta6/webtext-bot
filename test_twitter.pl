585778ccca81e64adec0306a3c68502d395174be
geta6 <getakura@gmail.com>
2014-01-13 01:48:42 +0900

    ニャーー!

--- test_twitter.pl
+++ test_twitter.pl
@@ -1,23 +1,67 @@
-#!/usr/bin/env perl
+#!/usr/bin/env perl -w
 
 use strict;
 use warnings;
 use utf8;
-use Net::Twitter;
-binmode STDOUT, ':utf8';
+use Text::CSV_XS;
+use Net::Twitter::Lite::WithAPIv1_1;
+use Data::Dumper;
+use Acme::Nyaa;
 
-my $ckey = '1yaX3KRSZh0l6E29MLN7xQ';
-my $csec = 'MJz4sP6iDE8gyputkYIfHNddldSzfC4NtJplCOFsUw';
-my $akey = '2255832258-dn9Z2lkDo4nRFg3wRLN9lWOLsM9BFHAQiwzhoEt';
-my $asec = '6EEKjxkkJSZUVDErgHvJS43dvM90YVZEZHQUKdP7lRaPu';
+my $txt = 'data.txt';
+my @ids = ();
 
-my $handle = Net::Twitter->new({
-    traits => [qw/OAuth API::RESTv1_1/],
-    consumer_key => $ckey,
-    consumer_secret => $csec,
-    access_token => $akey,
-    access_token_secret => $asec
-  });
+# file
 
-$handle->update({status=>'んほおおおおおお'});
+open my $fh, "<", $txt;
+while (my $line = readline $fh) {
+  chomp $line;
+  push @ids, $line;
+}
+close $fh;
+
+# acme
+
+my $nyaa = Acme::Nyaa->new;
+
+# twitter
+
+my $consumer_key = '1yaX3KRSZh0l6E29MLN7xQ';
+my $consumer_secret = 'MJz4sP6iDE8gyputkYIfHNddldSzfC4NtJplCOFsUw';
+my $token = '2255832258-dn9Z2lkDo4nRFg3wRLN9lWOLsM9BFHAQiwzhoEt';
+my $token_secret = '6EEKjxkkJSZUVDErgHvJS43dvM90YVZEZHQUKdP7lRaPu';
+
+my $nt = Net::Twitter::Lite::WithAPIv1_1->new(
+  consumer_key => $consumer_key,
+  consumer_secret => $consumer_secret,
+  access_token => $token,
+  access_token_secret => $token_secret
+);
+
+# search
+
+# 生協情報
+my $r = $nt->search({q => 'SFC 生協', lang => 'ja', count => 100, include_rts => 'false'});
+my $statuses = $r->{'statuses'};
+
+for my $status (@$statuses) {
+  my $text = $status->{text};
+  unless ($text =~ m/\ART\s/) {
+    if ( $text =~ m/[割閉休円]/ ) {
+      my $neko = $text;
+      $neko =~ s/\.\.*//g;
+      $neko =~ s/\(.*?\)//g;
+      $neko =~ s/【.*?】//g;
+      $neko =~ s/\A[@＠].*?[ 　]//g;
+      $neko = $nyaa->cat($neko);
+      print "$status->{created_at} <$status->{user}{screen_name}> $neko\n";
+    }
+  }
+}
+
+# 生協情報
+
+
+exit;
+
 
